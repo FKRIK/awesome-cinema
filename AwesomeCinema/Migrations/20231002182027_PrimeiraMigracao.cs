6a68df93261e239a0d1b834be0f2d139357c85be
FKRIK <fernandokrik@hotmail.com>
2023-10-02 15:25:16 -0300

    refatorado controller Sala para conectar ao banco. Feito primeira migracao

---
+++ AwesomeCinema/Migrations/20231002182027_PrimeiraMigracao.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AwesomeCinema.Migrations
+{
+    public partial class PrimeiraMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    FilmeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    Duracao = table.Column<int>(type: "INTEGER", nullable: false),
+                    AnoLancamento = table.Column<string>(type: "TEXT", nullable: true),
+                    Avaliacao = table.Column<float>(type: "REAL", nullable: false),
+                    EmCartaz = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.FilmeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    SalaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Assentos = table.Column<int>(type: "INTEGER", nullable: false),
+                    Disponivel = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.SalaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exibicoes",
+                columns: table => new
+                {
+                    ExibicaoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataHora = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FilmeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SalaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exibicoes", x => x.ExibicaoId);
+                    table.ForeignKey(
+                        name: "FK_Exibicoes_Filmes_FilmeId",
+                        column: x => x.FilmeId,
+                        principalTable: "Filmes",
+                        principalColumn: "FilmeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Exibicoes_Salas_SalaId",
+                        column: x => x.SalaId,
+                        principalTable: "Salas",
+                        principalColumn: "SalaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exibicoes_FilmeId",
+                table: "Exibicoes",
+                column: "FilmeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exibicoes_SalaId",
+                table: "Exibicoes",
+                column: "SalaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Exibicoes");
+
+            migrationBuilder.DropTable(
+                name: "Filmes");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+        }
+    }
+}
